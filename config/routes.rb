ae8787f956712caa59358566b1689b24531a566c
L <lukasz.czaplinski@tooploox.com>
2014-10-22 10:50:52 +0100

    added test for flash message

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'sessions/new'
 
+  root 'sessions#new'
   get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
