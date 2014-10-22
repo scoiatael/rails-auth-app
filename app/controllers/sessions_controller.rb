e40bf44d997885e041156f9aff37bb21042dfaca
L <lukasz.czaplinski@tooploox.com>
2014-10-22 10:52:18 +0100

    bugfix: login flash now disappears properly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       # Log the user in and redirect to the user's show page.
     else
       # Create an error message.
-      flash.alert = 'Invalid email/password combination' # Not quite right!
+      flash.now.alert = 'Invalid email/password combination'
       render 'new'
     end
   end
